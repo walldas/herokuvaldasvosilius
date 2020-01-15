942a8ec3721b7e10387985bbc56017d87d0c748d
walldas <88waldas@gmail.com>
2020-01-15 22:12:19 +0200

    kontaktai

--- main.py
+++ main.py
@@ -24,13 +24,30 @@ class User(db.Model):
 	password = db.Column(db.String(200), nullable=False)
 	image = db.Column(db.String(300), default="")
 	info = db.Column(db.String(1000), default="")
+	info_en = db.Column(db.String(1000), default="")
 	show = db.Column(db.String(10), default="False")
 	confirmed = db.Column(db.String(10), default="False")
+	
+	
+	
+class Contact_text(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	info = db.Column(db.String(4000), default="")
+	info_en = db.Column(db.String(4000), default="")
 
+	
+class Messages(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(200), nullable=False)
+	sms = db.Column(db.String(4000), default="")
+	
+	
 if not os.path.exists(app.config['SQLALCHEMY_DATABASE_URI']):
 	print(app.config['SQLALCHEMY_DATABASE_URI'])
 	db.create_all()
-
+	
 
 
 	
@@ -38,7 +55,9 @@ if not os.path.exists(app.config['SQLALCHEMY_DATABASE_URI']):
 @app.route("/")
 def index():
 	session_token = request.cookies.get("session_token")
+	print(">"*20)
 	print(session_token)
+	print(">"*20)
 	if session_token:
 		#user = db.query(User).filter_by(session_token=session_token).first() # kas per
 		user = "Valdas"
@@ -79,7 +98,15 @@ def registration():
 		return render_template("registration.html")
 		
 
-		
+@app.route("/kontaktai/", methods=["POST","GET"])
+def contacts_of_users():
+	users_ = reversed(User.query.order_by(User.date_created).all())
+	users = []
+	for user in users_:
+		if user.show == "True":
+			users.append(user)
+	return render_template("kontaktai.html", users=users)
+	
 		
 @app.route("/vartotojai/", methods=["POST","GET"])
 def control_users():
@@ -133,13 +160,13 @@ def edit_user(id):
 		user.surname = request.form['surname']
 		user.phone = request.form['phone']
 		user.email = request.form['email']
-		user.lvl = 0
+		#user.lvl = 0
 		password = request.form['password']
 		user.password = hashlib.sha256(password.encode()).hexdigest()
 		passwordr = request.form['passwordr']
 		#user.image = format(request.form['image'])
 		user.info = request.form['info']
-		user.show = "False"
+		#user.show = "False"
 		try:
 			db.session.commit()
 			return redirect('/vartotojai/')
@@ -162,6 +189,8 @@ def down_user(id):
 	except:
 		return 'problema mazinant vartotojo lvl'
 	
+	
+	
 @app.route('/up_user/<int:id>')
 def up_user(id):
 	user = User.query.get_or_404(id)
@@ -171,6 +200,30 @@ def up_user(id):
 		return redirect('/vartotojai/')
 	except:
 		return 'problema didinant vartotojo lvl'
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
